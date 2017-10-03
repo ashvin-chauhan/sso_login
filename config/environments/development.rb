7e300c1633978496fb25fe76875f3a19942a2b85
Tps <you@example.com>
2017-10-03 13:02:38 +0530

    Changes in url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  config.action_controller.relative_url_root = "/ror/sso_login"
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
